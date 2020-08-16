ac31d876b8504415eef018477a7cf7c8e4fcdbbf
Henry <henry@sensornetworks.co.za>
2020-08-16 19:14:09 +0200

    test flask session change

--- application.py
+++ application.py
@@ -3,7 +3,8 @@ import os
 import datetime;
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session.__init__ import Session
+from flask_session import Session
+
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
