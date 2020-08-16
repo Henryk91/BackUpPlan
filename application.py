989e31491da6db2070b92bf95feba4522a0149b1
Henry <henry@sensornetworks.co.za>
2020-08-16 19:11:01 +0200

    test flask session change

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import os
 import datetime;
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session
+from flask_session.__init__ import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -25,6 +25,8 @@ reminder_timer_array = []
 
 # Configure application
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or \
+    'e5ac358c-f0bf-11e5-9e39-d3b532c10a28'
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
